0a9343d0ae3dabc805dec1c2f5b49e453197a6fe
manuelmunoz-ct-uneat <manuel.munoz@ct.uneatlantico.es>
2026-04-06 16:03:33 +0200

    feat/layout para futuro componente

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
 from GUI.widget import ControlWidget, WordVisualizer
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 
 # Necesita una (y solo una) instancia de QApplication por aplicación.
 # Pase sys.argv para permitir argumentos de línea de comandos para su aplicación.
@@ -16,7 +16,12 @@ class MainWindow(QMainWindow):
         controlPanel1 = ControlWidget.ControlWidget("Controles")
         controlPanel2 = ControlWidget.ControlWidget("Visualizador")
 
-        self.setCentralWidget(WordVisualizer.WordUploadWidget())
+        vLayOut = QVBoxLayout()
+        vWidgetContainer = QWidget()
+        vLayOut.addWidget(WordVisualizer.WordUploadWidget())
+        
+        vWidgetContainer.setLayout(vLayOut)
+        self.setCentralWidget(vWidgetContainer)
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, controlPanel1)
         self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, controlPanel2)
 
