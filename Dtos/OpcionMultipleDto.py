f5ee59e907bc50959abbb80f9ae5412577fb56d5
manuelmunoz-ct-uneat <manuel.munoz@ct.uneatlantico.es>
2026-04-28 12:33:43 +0200

    fix/Dto innecesario

--- Dtos/OpcionMultipleDto.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from PreguntasDto import Pregunta
-from Dtos.RespuestaConValor import RespuestaConValorDto
-
-@dataclass(kw_only=True)
-class OpcionMultipleDto(Pregunta):
-    respuestas: RespuestaConValorDto
-
