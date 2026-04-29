d83f6f7ccd0c6b2ece163789d7e86983f536921d
manuelmunoz-ct-uneat <manuel.munoz@ct.uneatlantico.es>
2026-04-29 14:26:29 +0200

    fix/eliminacion de Dto inecesario

--- Dtos/VerdaderoFalsoDto.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from PreguntasDto import Pregunta
-from Dtos.RespuestaConValor import RespuestaConValor
-
-@dataclass(kw_only=True)
-class VerdaderoFalsoDto(Pregunta):
-    respuestas: RespuestaConValor
-
