4fdce4649aa7c0fc8773d9c798564a9bda3f482e
manuelmunoz-ct-uneat <manuel.munoz@ct.uneatlantico.es>
2026-04-29 14:27:34 +0200

    fix/Estructura de datos en Dtos

--- Dtos/PreguntasDto.py
+++ Dtos/PreguntasDto.py
@@ -3,9 +3,24 @@ from dataclasses import dataclass
 @dataclass
 class Pregunta:
     nombre_pregunta: str
-    enunciado_pregunta: str
+    preg: str
     tipo: str
-    nota_defecto: str
-    penalizacion: str
-    img: str | None
+    img: dict[str, str] | None
+
+    def __init__(self, data, id_pregunta):
+        id_pregunta = str(id_pregunta).zfill(3)
+        self.nombre_pregunta = f'PRB2547-POr-X01-{id_pregunta}'
+
+        self.preg = data.get('preg')
+        self.tipo = data.get('tipo')
+        self.img = {
+            'b64': data['img'].get('b64'),
+            'fmt': data['img'].get('fmt')
+            } if 'img' in data else None
+    
+    @staticmethod
+    def from_dict(data, id_pregunta):
+        return Pregunta(data, id_pregunta)
+        
+
 
--- Dtos/RespuestaDto.py
+++ Dtos/RespuestaDto.py
@@ -1,21 +1,31 @@
 from dataclasses import dataclass
 from typing import Dict
+from typing import Any
 
 @dataclass
 class OpcionDeRespuestaDto:
     txt: str
     retro: str
-    img: str | None
+    ok: bool
+    img: dict[str, str] | None
 
 
 @dataclass
 class RespuestaDto:
-    respuestas: Dict[str, OpcionDeRespuestaDto]
 
-    @staticmethod
-    def from_dict(data: dict) -> "RespuestaDto":
-        respuestas = {
-            key: OpcionDeRespuestaDto(**value)
-            for key, value in data.items()
-        }
-        return RespuestaDto(respuestas=respuestas)
+    def __init__(self, data: dict[str, Any]):
+        respuestas: Dict[str, OpcionDeRespuestaDto] = {}
+        for key in data.keys():
+            respuestas.update({
+                key: OpcionDeRespuestaDto(
+                    data.get("txt", ""),
+                    data.get("retro", ""),
+                    data.get("ok", ""),
+                    data.get("img")
+                    )
+        })
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(data)
+        
