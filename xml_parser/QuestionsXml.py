b99686b7a02864dde1d264f383effbfa68b935c1
manuelmunoz-ct-uneat <manuel.munoz@ct.uneatlantico.es>
2026-05-05 16:04:22 +0200

    feat/xml de pregunta de emparejamiento

--- xml_parser/QuestionsXml.py
+++ xml_parser/QuestionsXml.py
@@ -71,7 +71,60 @@ class QuestionXml:
     
     @staticmethod
     def prguntaEmparejamiento(pregunta: PreguntaEmparejamientoDto):
-        return
+        bloquePregunta = ""
+        bloquePregunta += (f"""
+<question type="matching">
+    <name>
+        <text>{pregunta.nombre_pregunta}</text>
+    </name>
+    <questiontext format="html">
+        <text>
+            <![CDATA[ <p>{pregunta.preg}</p> ]]>
+        </text>
+    </questiontext>
+    <generalfeedback format="html">
+        <text/>
+    </generalfeedback>
+    <defaultgrade>1.0000000</defaultgrade>
+    <penalty>0.3333333</penalty>
+    <hidden>0</hidden>
+    <idnumber/>
+    <shuffleanswers>true</shuffleanswers>
+    <correctfeedback format="html">
+        <text>
+            <![CDATA[ <p>Respuesta correcta</p> ]]>
+        </text>
+    </correctfeedback>
+    <partiallycorrectfeedback format="html">
+        <text>
+            <![CDATA[ <p>Respuesta parcialmente correcta.</p> ]]>
+        </text>
+    </partiallycorrectfeedback>
+    <incorrectfeedback format="html">
+        <text>
+            <![CDATA[ <p>Respuesta incorrecta.</p> ]]>
+        </text>
+    </incorrectfeedback>
+    <shownumcorrect/>
+        """)
+
+        for respuesta in pregunta.respuestas.respuestas.values():
+            bloque = ""
+            bloque += textwrap.dedent(f"""
+    <subquestion format="html">
+        <text>
+            <![CDATA[ <p>{respuesta.tabla.get("c0")}</p> ]]>
+        </text>
+        <answer>
+            <text>{respuesta.tabla.get("c1")}</text>
+        </answer>
+    </subquestion>
+            """).strip()
+            bloquePregunta += "\t" + bloque.replace("\n", "\n\t") + "\n"
+
+        bloquePregunta += "</question>\n"
+
+        return bloquePregunta
     
     @staticmethod
     def preguntaVerdaderoFalso(pregunta: PreguntaVerdaderoFalsoDto):
--- xml_parser/XmlParser.py
+++ xml_parser/XmlParser.py
@@ -9,16 +9,19 @@ class XmlParser:
 
         for question in questions:
             if isinstance(question, PreguntaEmparejamientoDto):
-                print("mayhaps")
+                preguntaEmparejamiento = QuestionXml.prguntaEmparejamiento(question)
+                xmlDocument += preguntaEmparejamiento
             elif isinstance(question, PreguntaEnsayoDto):
                 print(questions)
             elif isinstance(question, PreguntaOpcionMultipleDto):
                 cantidadCorrectas = sum(1 for op in question.respuestas.respuestas.values() if op.ok is True)
                 preguntaMulti = QuestionXml.preguntaOpcionMultiple(question, cantidadCorrectas)
                 xmlDocument += preguntaMulti
-                print(xmlDocument)
             elif isinstance(question, PreguntaVerdaderoFalsoDto):
                 print("mayhaps")
+            else:
+                print("crear nueva seccion de preguntas")
+                
 
         with open("documento_xml.xml", "w", encoding="utf-8") as file:
             file.write(xmlDocument)
