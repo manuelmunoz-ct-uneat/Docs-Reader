b52761ccb3cacfa1735becfa0d565479b637830c
manuelmunoz-ct-uneat <manuel.munoz@ct.uneatlantico.es>
2026-05-05 09:19:34 +0200

    feat/prubas de mapper

--- GUI/widget/WordVisualizer.py
+++ GUI/widget/WordVisualizer.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import (QApplication, QWidget, QPushButton, QTextEdit, QC
 from word_maneger.FileReader import FileReader
 from GUI.components.TextBox import JsonFormater
 from Dtos_Mapper.Mapper import Mapper
+from Dtos.RespuestasDto import RespuestaDto
 
 class WordUploadWidget(QWidget):
     FILE_FILTER = 'Word file (*.doc *.docx)'
@@ -37,7 +38,11 @@ class WordUploadWidget(QWidget):
                 fileLocation = self.getFileName()
                 document = FileReader(fileLocation)
                 paragraph = document.parse_to_json()
-                Mapper.tipoPregunta(dataJson=paragraph)
+                dtoData: list = Mapper.tipoPregunta(fileLocation, paragraph)
+                print(f"{dtoData}\n")
+                for obj in dtoData:
+                     if isinstance(obj, RespuestaDto):
+                        print(f"{obj.respuestas}\n")
                 self.textbox.setJson(paragraph)
             else:
                 print('Got Nothing')
