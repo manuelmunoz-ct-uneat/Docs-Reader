18b3836c8c36d3203de41b1166e487f49a70fcfc
manuelmunoz-ct-uneat <manuel.munoz@ct.uneatlantico.es>
2026-05-05 12:53:16 +0200

    feat/xml parser para preguntas de opcion multiple

---
+++ xml_parser/XmlParser.py
@@ -0,0 +1,25 @@
+from Dtos import (PreguntaEnsayoDto, PreguntaEmparejamientoDto, PreguntaOpcionMultipleDto, PreguntaVerdaderoFalsoDto)
+from.QuestionsXml import QuestionXml
+
+class XmlParser:
+    
+    @staticmethod
+    def parse(questions: list):
+        xmlDocument = ""
+
+        for question in questions:
+            if isinstance(question, PreguntaEmparejamientoDto):
+                print("mayhaps")
+            elif isinstance(question, PreguntaEnsayoDto):
+                print(questions)
+            elif isinstance(question, PreguntaOpcionMultipleDto):
+                cantidadCorrectas = sum(1 for op in question.respuestas.respuestas.values() if op.ok is True)
+                preguntaMulti = QuestionXml.preguntaOpcionMultiple(question, cantidadCorrectas)
+                xmlDocument += preguntaMulti
+                print(xmlDocument)
+            elif isinstance(question, PreguntaVerdaderoFalsoDto):
+                print("mayhaps")
+
+        with open("documento_xml.xml", "w", encoding="utf-8") as file:
+            file.write(xmlDocument)
+            
